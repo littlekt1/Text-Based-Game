7a08922468a71972c55734c8958d4be8c867e3b8
Alex Stone <aharrystone@gmail.com>
2023-04-08 10:40:47 -0500

    Added poison mechanic

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,5 +1,8 @@
+
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Player {
     //Instance Variables
@@ -7,6 +10,7 @@ public class Player {
     private String name;
     private int dexterity, strength, intelligence;
     private int currentLocation;
+    private int poisonLevel = 0;
     private boolean isAlive = true;
 
     public Player(String name) {
@@ -19,6 +23,92 @@ public class Player {
 
 
       //  this.inventory = getInventory();
+    }
+
+    //
+    public void poisonPlayer(){
+        poisonLevel = 1;
+        System.out.println("You've been infected! Find the antidote before it's too late!");
+    }
+
+    public void curePoison(){
+        poisonLevel = 0;
+        System.out.println("Ah, much better!");
+    }
+
+    public boolean incrementPoison(){
+        Random rand = new Random();
+        int randomNum = rand.nextInt((10 - 1) + 1) + 1;
+        if (randomNum > 7){
+            poisonLevel++;
+            poisonLevel++;
+            return true;
+        }
+        if (randomNum > 3){
+            poisonLevel++;
+            return true;
+        }
+        return false;
+    }
+
+
+    public void handlePoison() {
+        if (poisonLevel < 1) {
+            return;
+        }
+        if (!incrementPoison()){
+            return;
+        }
+
+        if (poisonLevel > 9) {
+            System.out.println("The infection has completely taken over, green stuff is pouring out of your eyes, mouth and nose.");
+            System.out.println("You are not long for this world... ");
+            System.out.println("ugh");
+            isAlive = false;
+            Application.deadEnd();
+        }
+        if (poisonLevel > 8) {
+            System.out.println("Green goo pours out of your wound.");
+            System.out.println("find the antitode immediately or die.");
+
+            if (intelligence >= 2) {
+                System.out.println("Your judgement is affected.");
+                intelligence--;
+            }
+
+        }
+        if (poisonLevel > 7) {
+            System.out.println("Your grievous wounds are beginning to catch up to you.");
+
+            if (intelligence >= 2) {
+                System.out.println("Your judgement is affected.");
+                intelligence--;
+            }
+        }
+
+        if (poisonLevel > 6) {
+            System.out.println("You are running out of time...");
+
+        }
+
+        if (poisonLevel > 5) {
+            System.out.println("The infection is getting serious...");
+            System.out.println("You feel feverish and have a strange craving for White Castle hamburgers");
+        }
+        if (poisonLevel > 4){
+            System.out.println("You feel..weirdly...hungry?  Odd.");
+        }
+        if (poisonLevel > 3){
+            System.out.println("Your wound hurts a lot more than it should... something ain't right.");
+        }
+        if (poisonLevel > 2){
+            System.out.println("Did something green just come out of your wound?  No, surely not.  Your eyes are playing tricks on you.");
+            System.out.println("That must be it.");
+        }
+        if (poisonLevel >= 1){
+            System.out.println("You feel a slight itching sensation coming from your wound...");
+        }
+
     }
 
     public String getName() {
